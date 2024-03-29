3b4a086dfb1590f2cb40afc971f4dcc981e2002b
Your Name <you@example.com>
2024-03-29 21:37:34 +0000

    new try

--- list.c
+++ list.c
@@ -118,7 +118,23 @@ void * popBack(List * list) {
 }
 
 void * popCurrent(List * list) {
-    return NULL;
+    if (list->current == NULL)
+        return NULL;
+    else
+    {
+        void * data = list->current->data;
+        if (list->current->prev == NULL)
+            list->head = list->current->next;
+        else
+            list->current->prev->next = list->current->next;
+        if (list->current->next == NULL)
+            list->tail = list->current->prev;
+        else
+            list->current->next->prev = list->current->prev;
+        free(list->current);
+        list->current = NULL;
+        return data;
+    }
 }
 
 void cleanList(List * list) {
